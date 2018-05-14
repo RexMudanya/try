fe2e31415934e0688a8ac5375d951352e5a0c2f9
Rex <musungumudanya@gmail.com>
2018-05-14 14:22:26 +0300

    First commit

---
+++ scatter.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+X=[456,567,654,345,323]
+Y=[567,678,323,659,222]
+
+plt.scatter(X,Y)
+
+plt.title('Rel Btwn temp and Coffee')
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+
+plt.show()
+
+
